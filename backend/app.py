5fce882aca008294f3f586d65670d87b6b140128
Diogo Matos Chaves <di.matoschaves@gmail.com>
2018-11-24 13:30:29 +0000

    reorganise folder structure and add frontend (React app)

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 print(os.environ['APP_SETTINGS'])
 
